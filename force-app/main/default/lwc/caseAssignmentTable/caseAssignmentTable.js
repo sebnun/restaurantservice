edc1a11c0182e3753bec01b0f10fc70c862d9109
Sebastian <sebnun@gmail.com>
2021-02-14 19:08:45 +0100

    Create classes, add field to User

--- force-app/main/default/lwc/caseAssignmentTable/caseAssignmentTable.js
+++ force-app/main/default/lwc/caseAssignmentTable/caseAssignmentTable.js
@@ -1,21 +1,83 @@
-import { LightningElement } from 'lwc';
-import fetchDataHelper from './fetchDataHelper';
+import { LightningElement, wire, track } from "lwc";
+import getCaseList from "@salesforce/apex/CaseHelper.getCaseList";
+import getUserList from "@salesforce/apex/UserHelper.getUserList";
 
-const columns = [
-  { label: 'Label', fieldName: 'name' },
-  { label: 'Website', fieldName: 'website', type: 'url' },
-  { label: 'Phone', fieldName: 'phone', type: 'phone' },
-  { label: 'Balance', fieldName: 'amount', type: 'currency' },
-  { label: 'CloseAt', fieldName: 'closeAt', type: 'date' },
-];
+const actions = [{ label: "Select user to Assign", name: "assign" }];
 
 export default class CaseAssignmentTable extends LightningElement {
-  data = [];
-  columns = columns;
+  isModalOpen = false;
 
-  // eslint-disable-next-line @lwc/lwc/no-async-await
-  async connectedCallback() {
-      const data = await fetchDataHelper({ amountOfRecords: 100 });
-      this.data = data;
+  openModal() {
+    this.isModalOpen = true;
   }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  assignUser() {
+    this.isModalOpen = false;
+
+    // TODO
+  }
+
+  caseColumns = [
+    {
+      label: "Case Number",
+      fieldName: "CaseNumber",
+      type: "text",
+      sortable: true
+    },
+    {
+      label: "Assignee",
+      fieldName: "Assignee__c",
+      type: "text",
+      sortable: true
+    },
+    {
+      label: "Assignment Status",
+      fieldName: "Assignment_Status__c",
+      type: "text",
+      sortable: true
+    },
+    {
+      type: "action",
+      typeAttributes: { rowActions: actions }
+    }
+  ];
+
+  @track error;
+  @track caseList;
+
+  @wire(getCaseList)
+  wiredCases({ error, data }) {
+    if (data) {
+      this.caseList = data;
+    } else if (error) {
+      this.error = error;
+    }
+  }
+
+  handleRowAction(event) {
+    const actionName = event.detail.action.name;
+    const row = event.detail.row;
+    switch (actionName) {
+      case "assign":
+        this.openModal();
+        break;
+      default:
+    }
+  }
+
+  @track userList;
+
+  @wire(getUserList)
+  wiredUsers({ error, data }) {
+    if (data) {
+      this.userList = data;
+    } else if (error) {
+      this.error = error;
+    }
+  }
+
 }
